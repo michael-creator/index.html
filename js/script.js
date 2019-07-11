db8985ad7fe3d970650c2dd7ff3b8f355791baf3
michael <mikelkarije@gmail.com>
2019-07-11 14:23:38 +0300

    Add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var add= function(number1, number2) {
-  return number1 * number2;
+  return number1 / number2;
 };
 var number1=parseInt(prompt("Enter number1"));
 var number2=parseInt(prompt("Enter number2"));
